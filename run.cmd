e4c48dc338700528ab398681db381fa01208c6bc
Vagner Lucas <vagnerlucas@institutoeducar.com.br>
2019-03-25 09:13:44 -0300

    adicionando scripts para rodar com volume

--- run.cmd
+++ run.cmd
@@ -3,10 +3,11 @@ setlocal
 
 set VOLUME=mywebserver
 set CONTAINER=vagnerlucas/webserver-linuxtips-exec
+set TAG=latest
 set VOLUME_FOLDER=/cygdrive/f/dump/myvol
 set PORT=8000
 
 echo Criando volume %VOLUME% ...
 docker volume create %VOLUME%
 echo Rodando container %CONTAINER% na porta %PORT%
-docker container run -p %PORT%:80 -d --mount type=volume,src=%VOLUME%,dst=%VOLUME_FOLDER% %CONTAINER%
+docker container run -p %PORT%:80 -d --mount type=volume,src=%VOLUME%,dst=%VOLUME_FOLDER% %CONTAINER%:%TAG%
