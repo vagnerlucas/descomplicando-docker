3dcdb722f8ab019846d0ee20c5d49296dbdd9f9f
Vagner Lucas <vagnerlucas@hotmail.com>
2019-03-29 13:32:09 -0300

    merging

--- build.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal 
-set TAG=1.0.0
-set IMAGE=webserver-linuxtips-exec
-
-echo "Criando imagem %IMAGE% : %TAG%"
-docker build -t %IMAGE%:%TAG% .
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TAG=1.0.0
-IMAGE=webserver-linuxtips-exec
-
-echo Criando imagem ${IMAGE} : ${TAG}
-docker build -t ${IMAGE}:${TAG} .
